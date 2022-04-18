4c70e78510ef83e9b94b8b0d0b72e28df6a40f8c
aristeu <aristeujose1999@gmail.com>
2022-04-18 18:30:17 -0300

    projeto finalizado

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="nav">
+      <router-link to="/" id="logo_url">
+          <img :src="logo" :alt="alt" id="logo">
+      </router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/Pedidos">Pedidos</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    props: ["logo", "alt"]
+}
+</script>
+
+<style scoped>
+    #nav {
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+    #nav #logo_url {
+        margin: auto auto auto 0;
+    }
+    #logo {
+        width: 40px;
+        height: 40px;
+    }
+    #nav a {
+        color: #FCBA03;
+        text-decoration: none;
+        margin: 12px;
+        transition: 0.5s;
+    }
+    #nav a:hover {
+        color: #FFF;
+    }
+</style>
